3ab7c848c3fc2aeccd33528729d5664113d8b4f1
Emilia Alves <enbrakaffe@gmail.com>
2024-05-05 00:52:10 -0300

    fixed blank spaces and changed the name of skipSpace function

--- Lexor.ts
+++ Lexor.ts
@@ -3,56 +3,58 @@ class Lexor {
   cur;
   col; 
   row; 
+  
 
-}
-parseApply(expr, program) {
-  program = skipSpace(program);
-  if (program[0] != "(") {
-    return {expr: expr, rest: program};
+  constructor(source) {
+    this.source = source;
+    this.cur, this.col, this.row = 0;
   }
-  program = skipSpace(program.slice(1));
-  expr = {type: "apply", operator: expr, args: []};
-  while (program[0] != ")") {
-    let arg = parseExpression(program);
-    expr.args.push(arg.expr);
-    program = skipSpace(arg.rest);
-    if (program[0] == ",") {
-      program = skipSpace(program.slice(1));
-    } else if (program[0] != ")") {
-      throw new SyntaxError("Expected ',' or ')'");
+
+  parseApply(expr, program) {
+    program = this.getNextToken(program);
+    if (program[0] != "(") {
+      return { expr: expr, rest: program };
     }
+    program = this.getNextToken(program.slice(1));
+    expr = { type: "apply", operator: expr, args: [] };
+    while (program[0] != ")") {
+      let arg = this.parseExpression(program);
+      expr.args.push(arg.expr);
+      program = this.getNextToken(arg.rest);
+      if (program[0] == ",") {
+        program = this.getNextToken(program.slice(1));
+      } else if (program[0] != ")") {
+        throw new SyntaxError("Expected ',' or ')'");
+      }
+    }
+    return this.parseApply(expr, program.slice(1));
   }
-  return parseApply(expr, program.slice(1));
-}
-parseExpression(program) {
-  program = skipSpace(program);
-  let match, expr;
-  if (match = /^([^"]*)"/.exec(program)) {
-    expr = {type : "value", value: match[1]};
-  } else if (match = /^\d+\b/.exec(program)) {
-    expr = {type : "value", value: Number(match[0])};
-  } else if (match = /^[^\s(),#"]+/.exec(program)) {
-    expr = {type: "word", name: match[0]};
-  } else {
-    throw new SyntaxError("Unexpected syntax: " + program);
+  parseExpression(program) {
+    program = this.getNextToken(program);
+    let match, expr;
+    if (match = /^([^"]*)"/.exec(program)) {
+      expr = { type: "value", value: match[1] };
+    } else if (match = /^\d+\b/.exec(program)) {
+      expr = { type: "value", value: Number(match[0]) };
+    } else if (match = /^[^\s(),#"]+/.exec(program)) {
+      expr = { type: "word", name: match[0] };
+    } else {
+      throw new SyntaxError("Unexpected syntax: " + program);
+    }
+
+    return this.parseApply(expr, program.slice(match[0].length));
   }
-
-  return parseApply(expr, program.slice(match[0].length));
-}
-
-skipSpace(string) { 
-  let first = string.search(/\S/); 
-  let isComment;
-  if (first == /\#/ || first == -1) return "";
-  return string.slice(first);
-} 
-
-parse(program) {
-  let {expr, rest} = parseExpression(program);
-  if (skipSpace(rest).length > 0) {
-    throw new SyntaxError("Unexpected text after program.");
+  getNextToken(string) {
+    let skipable = string.match(/^(\s|#.*)*/);
+    return string.slice(skipable[0].length);
   }
-  return expr;
+  parse(program) { 
+    let { expr, rest } = this.parseExpression(program);
+    if (this.getNextToken(rest).length > 0) {
+      throw new SyntaxError("Unexpected text after program.");
+    }
+    return expr;
+  }
 }
 
-console.log(parse("# hello\nx"));
+console.log(Lexor.prototype.parse("# hello\nx"));
--- lexor.js
+++ lexor.js
@@ -1,51 +1,57 @@
-parseApply(expr, program) {
-  program = skipSpace(program);
-  if (program[0] != "(") {
-    return {expr: expr, rest: program};
-  }
-    program = skipSpace(program.slice(1));
-    expr = {type: "apply", operator: expr, args: []};
-    while (program[0] != ")") {
-    let arg = parseExpression(program);
-    expr.args.push(arg.expr);
-    program = skipSpace(arg.rest);
-    if (program[0] == ",") {
-      program = skipSpace(program.slice(1));
-    } else if (program[0] != ")") {
-      throw new SyntaxError("Expected ',' or ')'");
+var Lexor = /** @class */ (function () {
+    function Lexor(source) {
+        this.source = source;
+        this.cur, this.col, this.row = 0;
     }
-  }
-  return parseApply(expr, program.slice(1));
-}
-parseExpression(program) {
-  program = skipSpace(program);
-  let match, expr;
-  if (match = /^([^"]*)"/.exec(program)) {
-    expr = {type : "value", value: match[1]};
-  } else if (match = /^\d+\b/.exec(program)) {
-    expr = {type : "value", value: Number(match[0])};
-  } else if (match = /^[^\s(),#"]+/.exec(program)) {
-    expr = {type: "word", name: match[0]};
-  } else {
-    throw new SyntaxError("Unexpected syntax: " + program);
-  }
-
-  return parseApply(expr, program.slice(match[0].length));
-}
-
-skipSpace(string) { 
-  let first = string.search(/\S/); 
-  let isComment;
-  if (first == /\#/ || first == -1) return "";
-  return string.slice(first);
-} 
-
-parse(program) {
-  let {expr, rest} = parseExpression(program);
-  if (skipSpace(rest).length > 0) {
-    throw new SyntaxError("Unexpected text after program.");
-  }
-  return expr;
-}
-
-console.log(parse("# hello\nx"));
+    Lexor.prototype.parseApply = function (expr, program) {
+        program = this.getNextToken(program);
+        if (program[0] != "(") {
+            return { expr: expr, rest: program };
+        }
+        program = this.getNextToken(program.slice(1));
+        expr = { type: "apply", operator: expr, args: [] };
+        while (program[0] != ")") {
+            var arg = this.parseExpression(program);
+            expr.args.push(arg.expr);
+            program = this.getNextToken(arg.rest);
+            if (program[0] == ",") {
+                program = this.getNextToken(program.slice(1));
+            }
+            else if (program[0] != ")") {
+                throw new SyntaxError("Expected ',' or ')'");
+            }
+        }
+        return this.parseApply(expr, program.slice(1));
+    };
+    Lexor.prototype.parseExpression = function (program) {
+        program = this.getNextToken(program);
+        var match, expr;
+        if (match = /^([^"]*)"/.exec(program)) {
+            expr = { type: "value", value: match[1] };
+        }
+        else if (match = /^\d+\b/.exec(program)) {
+            expr = { type: "value", value: Number(match[0]) };
+        }
+        else if (match = /^[^\s(),#"]+/.exec(program)) {
+            expr = { type: "word", name: match[0] };
+        }
+        else {
+            throw new SyntaxError("Unexpected syntax: " + program);
+        }
+        return this.parseApply(expr, program.slice(match[0].length));
+    };
+    Lexor.prototype.skipSpace = function (string) {
+        var skipable = string.match(/^(\s|#.*)*/);
+        return string.slice(skipable[0].length);
+    };
+    ;
+    Lexor.prototype.parse = function (program) {
+        var _a = this.parseExpression(program), expr = _a.expr, rest = _a.rest;
+        if (this.getNextToken(rest).length > 0) {
+            throw new SyntaxError("Unexpected text after program.");
+        }
+        return expr;
+    };
+    return Lexor;
+}());
+console.log(Lexor.prototype.parse("# hello\nx"));
